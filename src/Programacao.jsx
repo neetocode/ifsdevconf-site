ad84207b9ae3533e8a8912d3ca2835e2635c43e8
Miguel Neto <miguelrsn@gmail.com>
2018-02-27 07:25:27 -0300

    adicionando palestrantes

--- src/Programacao.jsx
+++ src/Programacao.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 
+import palestras from './data/palestras.json'
+
 
 class Programacao extends React.Component {
 
+    renderPalestra(palestra) {
+
+        return (
+            <div key={palestra.id} className="row">
+                <div className={`col-xs-12`}>
+                    <div className="box">
+                        <img className="palestrante-img" src={palestra.palestrante.img} />
+                        <h4 className="palestra-nome">{palestra.nome}</h4>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
-        return(
+        const palestrasDia03 = palestras.filter(palestra => palestra.dia == '03')
+
+        return (
             <section id="programacao">
 
-            <h2>PROGRAMAÇÃO</h2>
-            <h4> <i className="fa fa-cogs" aria-hidden="true"></i> Em construção</h4>
-        </section>
+                <h2>PROGRAMAÇÃO</h2>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <div className="box">
+                                <h3>03 Abril</h3>
+
+                                {palestrasDia03.map(this.renderPalestra.bind(this))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </section>
         )
     }
 }
