016f9b23149936e319cc45be9d4e1334103c1e75
Miguel Neto <miguelrsn@gmail.com>
2018-03-15 11:24:49 -0300

    add link repo github

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,19 @@ class App extends React.Component {
                 <Patrocinadores />
                 <Apoio />
                 <Contato />
+
+                <footer>
+                    <a target="_target" href="https://github.com/neetocode/ifsdevconf-site">
+
+                        <p className='text-center'>
+                            Contribua com o site
+                            {` `}
+                            <i className="fab fa-github"></i>
+                            <br />
+                            <i className="fas fa-heart" style={{ color: 'red' }}></i>
+                        </p>
+                    </a>
+                </footer>
             </div>
         )
     }
